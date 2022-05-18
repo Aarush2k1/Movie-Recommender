d881e3fd75adda0fe8a7d745c08948d7c51bd903
aarukansal2k1 <aarukansal2k1@gmail.com>
2022-05-18 17:50:55 +0530

    creating cf website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import pandas as pd
 
@@ -10,17 +10,16 @@ CORS(app)
 
 @app.route("/")
 def hello_from_root():
-    return jsonify(message='Hello from root!')
+    return render_template("index.html", data=similarity_df.columns.to_list())
 
 
-@app.route("/recommend", methods=["POST"])
+@app.route("/recommend", methods=["GET"])
 def make_rec():
-    if request.method == "POST":
-        data = request.json
-        movie = data["movie_title"]
+    if request.method == "GET":
+        data = request.args.get('title')
         try:
-            sim_score = similarity_df[movie]
-            sim_movies = sim_score.sort_values(ascending=False)[1:50]
+            sim_score = similarity_df[data]
+            sim_movies = sim_score.sort_values(ascending=False)[1:20]
             api_recommendations = sim_movies.index.to_list()
         except:
             api_recommendations = ['Movie not found']
